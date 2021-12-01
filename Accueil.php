9459ae85fac52b7062a6b7a8d403ce7f0d443ee8
Younes69 <92112459+Younes69@users.noreply.github.com>
2021-12-01 10:16:49 +0100

    Add files via upload

---
+++ Accueil.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="Accueil.css">
+    <script src="https://kit.fontawesome.com/afdb0ebcde.js" crossorigin="anonymous"></script>
+    <title>Accueil GSB</title>
+</head>
+<body><center>
+
+    <p>Bienvenue chez les laboratoires GSB</p>
+    <center>
+        <a href="login_connexion.php" class="lien_GSB"><i class="fas fa-euro-sign"></i><i class="fas fa-euro-sign"></i><i class="fas fa-euro-sign"></i> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Accéder à l'interface de gestion GSB &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i class="fas fa-euro-sign"></i><i class="fas fa-euro-sign"></i><i class="fas fa-euro-sign"></i></a>
+    </center>
+    
+</center></body>
+</html>
